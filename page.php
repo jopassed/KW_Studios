04426e67001c8dfc2e1685d9c11047d0edc8f586
Jo <jopassed@gmail.com>
2018-12-17 16:41:36 -0800

    header javasript carousel roughed in

--- page.php
+++ page.php
@@ -14,24 +14,62 @@
 get_header(); ?>
 <div id="page" class="hfeed site">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'studio' ); ?></a>
-<?php
+<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+ 
 
-    $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-echo '<header id="masthead" class="site-header" role="banner" style="background: url('. $url.') no-repeat 50% 50%;
-			-webkit-background-size: cover;
-			-moz-background-size:    cover;
-			-o-background-size:      cover;
-			background-size:         cover;
-            height:90vh;">';
+ <header id="masthead">
 
-?>
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <div class="site-header" role="banner" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/KWProduction.jpg') no-repeat 50% 50%;	-webkit-background-size: cover; -moz-background-size:cover;
+			-o-background-size: cover;
+			background-size: cover;
+            height:90vh;">'</div>
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <div class="site-header" role="banner" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/KWAtrium.jpg') no-repeat 50% 50%;	-webkit-background-size: cover; -moz-background-size:cover;
+			-o-background-size: cover;
+			background-size: cover;
+            height:90vh;">'</div>
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+   <div class="site-header" role="banner" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/images/kw-recording-for-web.png') no-repeat 50% 50%;	-webkit-background-size: cover; -moz-background-size:cover;
+			-o-background-size: cover;
+			background-size: cover;
+            height:90vh;">'</div>
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
 <div class="site-branding">
      <a href="#primary"><span class="typcn typcn-arrow-sorted-down move-on-down"></span></a>
 			<div class="site-branding-wrap">
                 <div id="site-header">
 			<h1 class="site-title ">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
+					<?php bloginfo( 'name' );?>
 				</a>
 			</h1>
 			
